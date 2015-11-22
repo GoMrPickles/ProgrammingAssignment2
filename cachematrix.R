e5cd4b0d5bead1cd526324d28e5b33c73807249c
GoMrPickles <GoMrPickles@users.noreply.github.com>
2015-11-22 08:11:05 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera R Programming
+## Assignment 2
+## Example code modified slightly - essentially working explicitly with matrices instead of vectors.
+## I don't really understand this code, but I understand how to modify code and specify a matrix.
+## So that's what I did.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a modification of the example code. I specify a matrix x,
+## and store it in a variable y outside of the definition of makeCacheMatrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+  m <- NULL
+
+## define the function "set" with argument y.
+## x is the argument to makeCacheMatrix, and it is stored as y.
+## m is defined as NULL in this function and outside it.
+## does << go one environment up, or globally? Is <<<- a thing?
+
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+
+## Same question as below - what's with the floating x?
+  get <- function() x
+
+## I don't understand these lines.
+## Like, why the floating "m" over there?
+  setmatrix <- function(solve) m <<- solve
+  getmatrix <- function() m
+  
+## I don't understand this list.
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of x using solve(x)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+
+        m <- x$getmatrix()
+
+## If m is not null, then the cached data is returned.
+## This can be verified by running something like "cacheSolve(someMatrix)" twice.
+## if the data is cached, the message will be shown and the cached data returned from m.
+
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+
+## assign the matrix specified in the argument to "matrix"
+  matrix <- x$get()
+  
+## solve the matrix specified in argument to the function and assign it to m
+  m <- solve(matrix, ...)
+
+## not sure what this line does.
+  x$setmatrix(m)
+
+## return m  
+  m
+
 }
